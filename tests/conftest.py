f555c4990ec526969055d118419375dd66a8ba8f
GladkiyAQA <d.gladkiy.aqa@gmail.com>
2025-08-25 18:30:36 +0200

    Фикстуры Pytest.Практика написания автотестов с использованием фикстур

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@ import pytest
 from pydantic import BaseModel, EmailStr
 
 from clients.authentication.authentication_client import AuthenticationClient, get_authentication_client
+from clients.private_http_builder import AuthenticationUserSchema
+from clients.users.private_users_client import get_private_users_client
 from clients.users.public_users_client import get_public_users_client, PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, UserSchema
 
@@ -18,6 +20,11 @@ class UserFixture(BaseModel):
     @property
     def password(self) -> str:  # Быстрый доступ к password пользователя
         return self.request.password
+
+    @property
+    def authentication_user(self) -> AuthenticationUserSchema:
+        """Модель для аутентификации клиентов (email + password)."""
+        return AuthenticationUserSchema(email=self.email, password=self.password)
 
 
 @pytest.fixture
@@ -37,3 +44,8 @@ def function_user(public_users_client: PublicUsersClient) -> UserFixture:
     request = CreateUserRequestSchema()
     response = public_users_client.create_user(request)
     return UserFixture(request=request, response=response)  # Возвращаем все нужные данные
+
+@pytest.fixture
+def private_users_client(function_user: UserFixture):
+    """Фикстура для аутентифицированного клиента пользователей."""
+    return get_private_users_client(function_user.authentication_user)
