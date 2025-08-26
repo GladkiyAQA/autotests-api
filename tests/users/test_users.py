a84e2c95904acc56831c006d76fdc691ad9728e4
GladkiyAQA <d.gladkiy.aqa@gmail.com>
2025-08-26 12:44:43 +0200

    Практикуемся в написании API автотестов. Часть 1.Практика написания негативных API автотестов для работы с файлами

--- tests/users/test_users.py
+++ tests/users/test_users.py
@@ -2,11 +2,14 @@ from http import HTTPStatus
 
 import pytest
 
+from clients.errors_schema import ValidationErrorResponseSchema
+from clients.files.files_client import FilesClient
 from clients.users.private_users_client import PrivateUsersClient
 from clients.users.public_users_client import PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
 from fixtures.users import UserFixture
 from tools.assertions.base import assert_status_code
+from tools.assertions.files import assert_get_file_with_incorrect_file_id_response
 from tools.assertions.schema import validate_json_schema
 from tools.assertions.users import assert_create_user_response, assert_get_user_response
 from tools.fakers import fake
@@ -37,4 +40,22 @@ class TestUsers:
         assert_status_code(response.status_code, HTTPStatus.OK)
         assert_get_user_response(response_data, function_user.response)
 
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
+    def test_get_file_with_incorrect_file_id(self, files_client: FilesClient):
+        """
+        Проверяет, что при запросе файла с некорректным UUID API
+        возвращает ошибку 422 и корректное сообщение о валидации.
+        """
+        # Отправляем запрос с некорректным file_id
+        response = files_client.get_file_api("incorrect-file-id")
+        response_data = ValidationErrorResponseSchema.model_validate_json(response.text)
+
+        # Проверяем статус-код (422 Unprocessable Entity)
+        assert_status_code(response.status_code, HTTPStatus.UNPROCESSABLE_ENTITY)
+
+        # Проверяем, что тело ответа соответствует ожидаемому
+        assert_get_file_with_incorrect_file_id_response(response_data)
+
+        # Проверяем JSON-схему
         validate_json_schema(response.json(), response_data.model_json_schema())
