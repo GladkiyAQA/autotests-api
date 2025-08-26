b07199509e267092e636524f20478ba95f0afa8b
GladkiyAQA <d.gladkiy.aqa@gmail.com>
2025-08-26 14:32:56 +0200

    Практикуемся в написании API автотестов. Часть 3.Практика написания API автотестов для работы с заданиями.Получение задания

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -2,11 +2,13 @@ from http import HTTPStatus
 import pytest
 
 from clients.exercises.exercises_client import ExercisesClient
-from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
+    GetExerciseResponseSchema
 from fixtures.courses import CourseFixture
+from fixtures.exercises import ExerciseFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
-from tools.assertions.exercises import assert_create_exercise_response
+from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response
 
 
 @pytest.mark.exercises
@@ -34,6 +36,29 @@ class TestExercises:
 
         # Проверяем тело ответа
         assert_create_exercise_response(request, response_data)
+
+        # Проверяем JSON-схему
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
+    def test_get_exercise(
+            self,
+            exercises_client: ExercisesClient,
+            function_exercise: ExerciseFixture
+    ):
+        """
+        Проверяет получение задания по его ID через API.
+        """
+        # Отправляем запрос к эндпоинту /exercises/{exercise_id}
+        response = exercises_client.get_exercise_api(function_exercise.response.exercise.id)
+
+        # Десериализация ответа
+        response_data = GetExerciseResponseSchema.model_validate_json(response.text)
+
+        # Проверяем статус-код
+        assert_status_code(response.status_code, HTTPStatus.OK)
+
+        # Проверяем тело ответа
+        assert_get_exercise_response(response_data, function_exercise.response)
 
         # Проверяем JSON-схему
         validate_json_schema(response.json(), response_data.model_json_schema())
--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -1,4 +1,5 @@
-from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
+    ExerciseSchema, GetExerciseResponseSchema
 from tools.assertions.base import assert_equal
 
 
@@ -21,3 +22,33 @@ def assert_create_exercise_response(
     assert_equal(exercise.max_score, request.max_score, "max_score")
     assert_equal(exercise.min_score, request.min_score, "min_score")
     assert_equal(exercise.estimated_time, request.estimated_time, "estimated_time")
+
+def assert_exercise(actual: ExerciseSchema, expected: ExerciseSchema):
+    """
+    Проверяет, что фактические данные задания соответствуют ожидаемым.
+
+    :param actual: Фактические данные (ExerciseSchema).
+    :param expected: Ожидаемые данные (ExerciseSchema).
+    :raises AssertionError: Если данные не совпадают.
+    """
+    assert_equal(actual.id, expected.id, "id")
+    assert_equal(actual.course_id, expected.course_id, "course_id")
+    assert_equal(actual.title, expected.title, "title")
+    assert_equal(actual.description, expected.description, "description")
+    assert_equal(actual.max_score, expected.max_score, "max_score")
+    assert_equal(actual.min_score, expected.min_score, "min_score")
+    assert_equal(actual.estimated_time, expected.estimated_time, "estimated_time")
+
+
+def assert_get_exercise_response(
+        get_exercise_response: GetExerciseResponseSchema,
+        create_exercise_response: CreateExerciseResponseSchema
+):
+    """
+    Проверяет, что ответ на получение задания соответствует ответу на его создание.
+
+    :param get_exercise_response: Ответ API при GET-запросе задания.
+    :param create_exercise_response: Ответ API при создании задания.
+    :raises AssertionError: Если данные не совпадают.
+    """
+    assert_exercise(get_exercise_response.exercise, create_exercise_response.exercise)
