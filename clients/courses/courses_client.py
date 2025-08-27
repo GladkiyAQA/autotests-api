1b251a6244842b3b8ab88a4d1987b8e0bb1b5f3f
GladkiyAQA <d.gladkiy.aqa@gmail.com>
2025-08-27 22:41:50 +0200

    Добавление Allure шагов в автотесты

--- clients/courses/courses_client.py
+++ clients/courses/courses_client.py
@@ -1,3 +1,4 @@
+import allure
 from httpx import Response
 
 from clients.api_client import APIClient
@@ -16,6 +17,7 @@ class CoursesClient(APIClient):
     Клиент для работы с /api/v1/courses
     """
 
+    @allure.step("Get courses")
     def get_courses_api(self, query: GetCoursesQuerySchema) -> Response:
         """
         Метод получения списка курсов.
@@ -25,6 +27,7 @@ class CoursesClient(APIClient):
         """
         return self.get("/api/v1/courses", params=query.model_dump(by_alias=True))
 
+    @allure.step("Get course by id {course_id}")
     def get_course_api(self, course_id: str) -> Response:
         """
         Метод получения курса.
@@ -34,6 +37,7 @@ class CoursesClient(APIClient):
         """
         return self.get(f"/api/v1/courses/{course_id}")
 
+    @allure.step("Create course")
     def create_course_api(self, request: CreateCourseRequestSchema) -> Response:
         """
         Метод создания курса.
@@ -44,7 +48,7 @@ class CoursesClient(APIClient):
         """
         return self.post("/api/v1/courses", json=request.model_dump(by_alias=True, mode="json"))
 
-
+    @allure.step("Update course by id {course_id}")
     def update_course_api(self, course_id: str, request: UpdateCourseRequestSchema) -> Response:
         """
         Метод обновления курса.
@@ -55,6 +59,7 @@ class CoursesClient(APIClient):
         """
         return self.patch(f"/api/v1/courses/{course_id}", json=request.model_dump(by_alias=True, exclude_none=True))
 
+    @allure.step("Delete course by id {course_id}")
     def delete_course_api(self, course_id: str) -> Response:
         """
         Метод удаления курса.
